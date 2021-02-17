bb39d432dfd97cac58470fab6bb00165168414ed
Ömer Faruk Koç <77229286+Fcyb3r@users.noreply.github.com>
2021-02-17 12:10:52 +0300

    tekliOperatorlar1

---
+++ fcyber/src/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 int x, y, z, m;
+
+	        x=10; 
+	        y=5;
+	        z=16;
+	        
+	        m=++x - --y + z++;
+	        
+	        
+	        
+	        /*
+	         * 11-4+16=23=m
+	         * 
+	         * 
+	         * 
+	         * 
+	         */
+	        System.out.println("z="+z);
+	        System.out.println("x="+x);
+	        System.out.println("y="+y);
+	        System.out.println("m="+m);
+	        
+	}
+}
